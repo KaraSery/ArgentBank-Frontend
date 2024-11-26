2ee1efa79366d8ffc297e19fdc80476fc414e4c6
Noreyni Sery <69noreyni.sery@gmail.com>
2024-11-26 11:31:14 +0100

    Remove unused assets

--- src/pages/LogIn/LogIn.tsx
+++ src/pages/LogIn/LogIn.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LogInForm from "../../features/auth/LogInForm/LogInForm";
 
 export default function LogIn() {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -9,22 +10,7 @@ export default function LogIn() {
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
-                <form id='login-form' data-testid="sign-in-form" onSubmit={handleSubmit}>
-                    <div className="input-wrapper">
-                        <label htmlFor="username">Username</label
-                        ><input required data-testid="username-input" type="email" id="email" name="email"/>
-                    </div>
-                    <div className="input-wrapper">
-                        <label htmlFor="password">Password</label
-                        ><input required data-testid="password-input" type="password" id="password" name="password"/>
-                    </div>
-                    <div className="input-remember">
-                        <input type="checkbox" id="remember-me"/><label htmlFor="remember-me"
-                    >Remember me</label
-                    >
-                    </div>
-                    <button data-testid="submit-button" className="sign-in-button">Sign In</button>
-                </form>
+                <LogInForm/>
             </section>
         </>
     )
