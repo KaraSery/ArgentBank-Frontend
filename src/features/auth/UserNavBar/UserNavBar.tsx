ddeaef33c633e387969cde1943b3d37b16928882
Noreyni Sery <69noreyni.sery@gmail.com>
2024-12-06 09:16:26 +0100

    Optimize image by converting to WebP

--- src/features/auth/UserNavBar/UserNavBar.tsx
+++ src/features/auth/UserNavBar/UserNavBar.tsx
@@ -18,7 +18,7 @@ export default function UserNavBar() {
       <Link className="main-nav-logo" to="">
         <img
           className="main-nav-logo-image"
-          src="/img/argentBankLogo.png"
+          src="/img/argentBankLogo.webp"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
